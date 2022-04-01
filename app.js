b7063cb95d5043198bef05888d1d53a1088ad115
aswarth <m.aswarthreddy@gmail.com>
2022-04-01 10:34:54 +0530

    id

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 function download(){
 
-  var audio = new Audio('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  var audio = new Audio('/audio.mp3');
   audio.play();
   //window.open('/lol.txt')
 }
