869eb1f6d1260fdb30893905bc60a060e73cee94
aswarth <m.aswarthreddy@gmail.com>
2022-04-01 10:53:55 +0530

    idk

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 function download(){
 
-  var audio = new Audio('IDEA-Lab/audio.mp3');
+  var audio = new Audio('https://github.com/aswarthm/IDEA-Lab/blob/main/audio.mp3');
   audio.play();
   //window.open('/lol.txt')
 }
