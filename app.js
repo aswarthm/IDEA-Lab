db0294c3196c51dfed8da257bc54dbebe3b1c797
aswarth <m.aswarthreddy@gmail.com>
2022-04-01 09:10:42 +0530

    fix link

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ function download(){
 
   var audio = new Audio('https://github.com/aswarthm/IDEA-Lab/blob/main/audio.mp3');
   audio.play();
-  window.open('/lol.txt')
+  window.open('https://github.com/aswarthm/IDEA-Lab/blob/main/lol.txt')
 }
